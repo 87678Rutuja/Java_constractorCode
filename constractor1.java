5a40d703be7dcfcb18f47cffbf30cae478063a16
87678Rutuja <132226247+87678Rutuja@users.noreply.github.com>
2023-08-14 20:54:08 +0530

    Add files via upload

---
+++ constractor1.java
@@ -0,0 +1,15 @@
+class Demo{
+	Demo(){
+		System.out.println("In Demo");
+	}
+}
+	class IPL{
+		IPL(){
+			System.out.println("ML");
+		}
+		public static void main(String[] ar){
+			IPL obj1 = new IPL();
+			 IPL obj2 = new IPL();
+		}
+	}
+
